23ea69dc0bf1a5fd79736e4206a27e46274a2597
Derick Vitancol <derickvitancol@gmail.com>
2019-10-18 15:42:29 +0800

    item class

--- item.cs
+++ item.cs
@@ -8,6 +8,15 @@ namespace COE131L
 {
     class item
     {
-       
+        public int serialNumber;
+        public int itemType;
+        public int addedby;
+        public int conditionId;
+        public int statusId;
+
+        public string supplier;
+
+        public DateTime datedelivered;
+        public DateTime datedecomm;
     }
 }
